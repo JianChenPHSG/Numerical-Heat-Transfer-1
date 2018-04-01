828486f0d95060228b4f2b4090ce2b3880939db0
Fishermanljh <33857942+Fishermanljh@users.noreply.github.com>
2018-04-02 04:29:04 +0800

    'commit'

---
+++ Teamwork1/Teamwork1/Debug/center_d__genmod.f90
@@ -0,0 +1,9 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sun Apr 01 20:10:12 2018
+        MODULE CENTER_D__genmod
+          INTERFACE 
+            SUBROUTINE CENTER_D(N,VELO)
+              INTEGER(KIND=4) :: N
+              REAL(KIND=4) :: VELO
+            END SUBROUTINE CENTER_D
+          END INTERFACE 
+        END MODULE CENTER_D__genmod
