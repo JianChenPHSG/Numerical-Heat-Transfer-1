7c491306cecd4c17c35464630c4a3872455bf0fb
LeoMa_thu <1821774743@qq.com>
2018-04-07 10:52:52 +0800

    加入了两种TDMA算法（代码更短，可调用进行使用），加入了考虑边界条件的Center_Format

--- Teamwork1/Teamwork1/main.f90
+++ Teamwork1/Teamwork1/main.f90
@@ -301,23 +301,112 @@ END DO
 RETURN
 END SUBROUTINE 
 
-! 标准TDMA算法，还没有加入到里面，需要确定一下
-!TRI-DIAGONAL SOLUTION ALGORITHM
-!NOTE ARRAYS B AND D ARE DESTROYED IN THE PROCESS.
-!FOR THE SOLUTION OF:     -C*T(I-1) + A*T(I) - B*T(I+1) = D
-SUBROUTINE TDMA(A,B,C,D,N,X)
-	IMPLICIT REAL*8 (A-H,O-Z)
-	REAL*8 A(1),B(1),C(1),D(1),X(1)
-	B(1)=B(1)/A(1)
-	D(1)=D(1)/A(1)
-	DO I = 2 , N
-	    B(I)=B(I)/(A(I)-C(I)*B(I-1))
-	    D(I)=(D(I)+C(I)*D(I-1))/(A(I)-C(I)*B(I-1))
-     ENDDO
-	X(N)=D(N)
-	DO I = N-1 , 1 , -1                          !step size = -1
-	    X(I)=B(I)*X(I+1)+D(I)
-     ENDDO
-	RETURN
-	END
 
+
+!Center Format
+!mayugao edit
+SUBROUTINE Center_Format(N,Velo)
+REAL::Velo
+INTEGER::N
+REAL::Len=1,Dens=1,Gama=0.1,D,F,Delta_x
+REAL,ALLOCATABLE::A_e(:),A_p(:),A_w(:),X(:),A_c(:),d_(:),c_(:)
+
+
+ALLOCATE(A_e(N))
+ALLOCATE(A_p(N))
+ALLOCATE(A_w(N))
+ALLOCATE(A_c(N))
+ALLOCATE(X(N))
+
+ALLOCATE(d_(N))
+ALLOCATE(c_(N))
+
+Delta_x=Len/N
+D=Gama/Delta_x
+F=Dens*Velo 
+
+!以下是马誉高推导的边界条件
+A_e(1)=D-F/2
+A_w(1)=0
+A_p(1)=D+3*F/2
+A_c(1)=(F+2*D)
+
+A_e(N)=0
+A_w(N)=D+F/2
+A_p(N)=3*D+F/2
+A_c(N)=0
+
+!以下是冯夷宁组的边界条件
+!A_e(1)=(D-F/2)
+!A_w(1)=0
+!A_p(1)=2*D
+!A_c(1)=(D+F/2)
+
+!A_e(N)=0
+!A_w(N)=(D+F/2)
+!A_p(N)=2*D
+!A_c(N)=0
+
+DO i=2,N-1,1
+!中心差分的系数
+A_e(i)=(D-F/2)
+A_w(i)=(D+F/2)
+A_p(i)=2*D
+A_c(i)=0
+END DO
+
+
+call TDMA(A_p,A_e,A_w,A_c,N,X)
+write(*,*) A_p,A_e,A_w,A_c,N,X
+
+OPEN(UNIT=1,FILE='Center_D.txt')
+DO i=1,N,1
+WRITE(1,*) X(i)
+END DO
+RETURN
+END SUBROUTINE 
+
+! TDMA算法
+! TRI-DIAGONAL SOLUTION ALGORITHM
+! NOTE: ARRAYS B AND D ARE DESTROYED IN THE PROCESS.
+! FOR THE SOLUTION OF:     -C*T(I-1) + A*T(I) - B*T(I+1) = D
+! 改编自柯道友老师的讲解版本
+! 可运行
+SUBROUTINE TDMA(A_p,A_e,A_w,A_c,N,X)
+    INTEGER::N
+    REAL :: A_e(N),A_p(N),A_w(N),X(N),A_c(N)
+
+
+	A_e(1)=(A_e(1))/(A_p(1))
+	A_c(1)=A_c(1)/(A_p(1))
+	DO I = 2 , N
+	    A_e(I)=A_e(I)/((A_p(I))-A_w(I)*A_e(I-1))
+	    A_c(I)=(A_c(I)+A_w(I)*A_c(I-1))/((A_p(I))-A_w(I)*A_e(I-1))
+    ENDDO
+	X(N)=A_c(N)
+	DO I = N-1 , 1 , -1                          !step size = -1
+	    X(I)=A_e(I)*X(I+1)+A_c(I)
+    ENDDO
+END SUBROUTINE
+
+
+! A_p*T(I) = A_w*T(I-1)+A_e*T(I+1)+A_c
+! TDMA求解方程
+! 改编自冯夷宁版本
+! 可运行，无差别
+SUBROUTINE TDMA2(A_p,A_e,A_w,A_c,N,X)
+    INTEGER::N
+    REAL :: A_e(N),A_p(N),A_w(N),X(N),A_c(N),c_(N),d_(N)
+
+    c_(1)=-A_e(1)/A_p(1)
+    d_(1)=A_c(1)/A_p(1)
+    DO I=2,(N-1)
+        c_(I)=(-A_e(I))/(A_p(I)-c_(I-1)*(-A_w(I)))
+        d_(I)=(A_c(I)-d_(I-1)*(-A_w(I)))/(A_p(I)-c_(I-1)*(-A_w(I)))
+    ENDDO
+        d_(N)=(A_c(N)-d_(N-1)*(-A_w(N)))/(A_p(N)-c_(N-1)*(-A_w(N-1)))
+        X(N)=d_(N)
+    DO I=1,(N-1)
+        X(N-I)=d_(N-I)-c_(N-I)*X(N-I+1)
+    ENDDO
+END SUBROUTINE
